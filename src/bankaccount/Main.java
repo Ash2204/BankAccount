0d57e0098eee533ad12f54fb738a2925f53b0cc6
Ash2204 <23323381+Ash2204@users.noreply.github.com>
2018-01-19 13:22:44 +0000

    Added the first menu of the application and included the buffered reader to read the users menu choices. Also fixed imports

--- src/bankaccount/Main.java
+++ src/bankaccount/Main.java
@@ -1,15 +1,27 @@
 
 package bankaccount;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Ashley
  */
 public class Main {
+    
+    
+                  public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    /**
-     * @param args the command line arguments
-     */
+	public static void printMenu() {
+		System.out.println("1. Create Bank Account");
+		System.out.println("2. Create User");
+		System.out.println("3. Run Simulation");
+		System.out.println("4. Exit");
+		System.out.println(">> ");
+	}
+
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
