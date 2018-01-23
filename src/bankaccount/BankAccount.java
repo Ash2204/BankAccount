597727d977ca027c22ddd10485c7058b1c52d9bc
Ash2204 <23323381+Ash2204@users.noreply.github.com>
2018-01-23 22:50:35 +0000

    Added both withdraw method and transaction method implementing runnable.

--- src/bankaccount/BankAccount.java
+++ src/bankaccount/BankAccount.java
@@ -31,4 +31,35 @@ public class BankAccount {
 		System.out.println("Operation for deposit  >> name:" + user.getUserName() + ", surname : " + user.getUserSurname()
 						+ ", value => " + value + ", Account Balance = " + user.getBankAccount().accountBalance);
 		return true;
+                 
+                  }
+
+	public boolean withdraw(double value, user user) {
+
+		if (user.getBankAccount().accountBalance < value) {
+			System.out.println("Can't withdraw (user:" + user.getUserName() + ",maney:" + String.valueOf(value)
+					+ ")! Current account haven't sufficient");
+			return false;
+		}
+		user.getBankAccount().accountBalance -= value;
+		System.out.println();
+		System.out.println("Operation for withdraw >> name:" + user.getUserName() + ", surname : " + user.getUserSurname()
+						+ ", value => " + value + ", Account Balance = " + user.getBankAccount().accountBalance);
+		return true;
+	}
+}
+
+class Transaction implements Runnable {
+	private BankAccount sourceAccount, destinationAccount;
+	private double amount;
+
+	Transaction(BankAccount sourceAccount, BankAccount destinationAccount, double amount) {
+		this.sourceAccount = sourceAccount;
+		this.destinationAccount = destinationAccount;
+		this.amount = amount;
+	}
+
+	public void run() {
+		System.out.printf("%s completed ", Thread.currentThread().getName());
+	}
 }
