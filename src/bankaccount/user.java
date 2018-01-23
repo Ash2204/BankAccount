8ec4e136f201911ee58d1563c827ff5157e120f3
Ash2204 <23323381+Ash2204@users.noreply.github.com>
2018-01-23 16:01:49 +0000

    Added user class, for name, surname, bankAccount and transactionList

---
+++ src/bankaccount/user.java
@@ -0,0 +1,34 @@
+
+package bankaccount;
+
+/**
+ *
+ * @author Ashley
+ */
+public class user {
+	private String name;
+	private String surname;
+	private final BankAccount bankAccount;
+	private final double[] transactionList;
+
+	public user(String name, String surname, BankAccount bA, double[] tL) {
+		this.name = name;
+		this.surname = surname;
+		this.bankAccount = bA;
+		this.transactionList = tL;
+	}
+
+	public String getUserName() {
+		return this.name;
+	}
+
+	public BankAccount getBankAccount() {
+		return this.bankAccount;
+	}
+
+	public String getUserSurname() {
+		return this.surname;
+	}
+
+}
+
