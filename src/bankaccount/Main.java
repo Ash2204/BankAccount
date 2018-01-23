d47ed702b521b27ed7ae6e49d14065ce1d692766
Ash2204 <23323381+Ash2204@users.noreply.github.com>
2018-01-23 15:08:34 +0000

    Create users, with names, surnames and a array of transactions set in the work sheet.

--- src/bankaccount/Main.java
+++ src/bankaccount/Main.java
@@ -62,6 +62,30 @@ public class Main {
 
 		return null;
 	}
+                 
+                  // creates the users defines names, surnames and transactions in array list
+                  public static user createUser() {
+		String name1 = "Saul", surname1 = "Goodman";
+		String name2 = "Walter", surname2 = "White";
+		String name3 = "Jessie", surname3 = "Pinkman";
+		String name4 = "Hank", surname4 = "Schrader";
+
+		BankAccount userBank1 = createAccount();
+		BankAccount userBank2 = createAccount();
+		BankAccount userBank3 = createAccount();
+		BankAccount userBank4 = createAccount();
+
+		double[] transaction1 = { 50, 10, -20, 10, -20, 20, 10, 50, -10, 10, -10, 50 };
+		double[] transaction2 = { 20, 20, -20, 50, -20, 10, 50, 50, -20, 10, 10 };
+		double[] transaction3 = { 50, 10, 10, -10, -10, 50, 20, -10, -1111 };
+		double[] transaction4 = { 50, 10, -20, 20, 10, -20 };
+
+		user user1 = new user(name1, surname1, userBank1, transaction1);
+		user user2 = new user(name2, surname2, userBank2, transaction2);
+		user user3 = new user(name3, surname3, userBank3, transaction3);
+		user user4 = new user(name4, surname4, userBank4, transaction4);
+
+		return null;
                   
                   
     public static void main(String[] args) {
