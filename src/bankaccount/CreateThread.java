1b796498794d6b09a229839e012280b184ec4ca1
Ash2204 <23323381+Ash2204@users.noreply.github.com>
2018-01-24 09:16:19 +0000

    Added runnable to Create Thread using implements and created a loop for  the transaction list the user creates.

--- src/bankaccount/CreateThread.java
+++ src/bankaccount/CreateThread.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bankaccount;
 
 /**
  *
  * @author Ashley
  */
-public class CreateThread {
-    
+
+public class CreateThread implements Runnable {
+
+	private User user;
+	private double[] transaction;
+	private BankAccount userBank;
+	
+	
+	@Override
+	public void run() {
+		// Wait to simulate io like database access ...
+		
+		for (int i = 0; i < transaction.length; i++) {
+			if (transaction[i] > 0) {
+				userBank.deposit(transaction[i], user);
+			} else {
+				if (!userBank.withdraw(-transaction[i], user)) {
+					
+				}
+			}
+			try {
+				Thread.sleep(100);
+			}
+			catch(Exception e) {
+				
+			}
+		}
+		
+	}
+
+	public CreateThread(User user, BankAccount userBank, double[] transaction) {
+		this.user = user;
+		this.userBank = userBank;
+		this.transaction = transaction;
+	}
+	
 }
+
