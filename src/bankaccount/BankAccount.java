62652c028b5608004c6463c2408da7e4be32a9f4
Ash2204 <23323381+Ash2204@users.noreply.github.com>
2018-01-24 12:30:40 +0000

    Added Locks in comments to swap between synchronized and locks

--- src/bankaccount/BankAccount.java
+++ src/bankaccount/BankAccount.java
@@ -8,7 +8,7 @@ public class BankAccount {
                  
                  private long accountNo;
 	private double accountBalance;
-	
+	// Lock lock = new ReentrantLock();
 
 	public BankAccount(long accountNo, double accountBalance) {
 		this.accountNo = accountNo;
@@ -24,17 +24,20 @@ public class BankAccount {
 	}
         
                   public synchronized boolean deposit(double value, User user) {
-		
+		// lock.lock();
 		user.getBankAccount().accountBalance += value;
+                                    // System.out.println(user.getBankAccount().accountBalance + " => " +
+		// value)
 		System.out.println();
 		System.out.println("Operation for deposit  >> name:" + user.getUserName() + ", surname : " + user.getUserSurname()
 						+ ", value => " + value + ", Account Balance = " + user.getBankAccount().accountBalance);
-		return true;
+		// lock.unlock();
+                                    return true;
                  
                   }
 
 	public synchronized boolean withdraw(double value, User user) {
-
+                                   // lock.lock();
 		if (user.getBankAccount().accountBalance < value) {
 			System.out.println("Can't withdraw (user:" + user.getUserName() + ",money:" + String.valueOf(value)
 					+ ")! Your current account has insufficient funds");
@@ -42,9 +45,12 @@ public class BankAccount {
 		}
 		user.getBankAccount().accountBalance -= value;
 		System.out.println();
+                                   // System.out.println(user.getBankAccount().accountBalance + " => " +
+		// value);
 		System.out.println("Operation for withdraw >> name:" + user.getUserName() + ", surname : " + user.getUserSurname()
 						+ ", value => " + value + ", Account Balance = " + user.getBankAccount().accountBalance);
-		return true;
+		// lock.unlock();
+                                    return true;
 	}
 }
 
